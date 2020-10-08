dd56ff94e0acebdcff284470adcb1acdbed0fbb8
AugmenTab <tbaum92@live.com>
2020-10-07 20:49:12 -0600

    Completed input normalization advanced task.

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -2,15 +2,20 @@ package edu.cnm.deepdive;
 
 public class Palindromes {
 
-  public static boolean computeRecursive(String phrase) {
+  public static boolean testRecursive(String phrase) {
     return phrase.length() <= 1
         || (
             phrase.charAt(0) == phrase.charAt(phrase.length() - 1)
-            && computeRecursive(phrase.substring(1, phrase.length() - 1))
+            && testRecursive(phrase.substring(1, phrase.length() - 1))
         );
   }
 
-  public static boolean computeIterative(String phrase) {
+  public static boolean testDenormalized(String phrase) {
+    String formattedString = phrase.replaceAll("[\\W_]+", "").toLowerCase();
+    return testRecursive(formattedString);
+  }
+
+  public static boolean testIterative(String phrase) {
     boolean isPalindrome = true;
     for (int i = 0; i < (int)Math.floor(phrase.length() / 2); i++) {
       if (phrase.charAt(i) != (phrase.charAt(phrase.length() - (i + 1)))) {
--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.SQLOutput;
 import org.junit.jupiter.api.Test;
 
 public class PalindromesTest {
@@ -14,17 +15,36 @@ public class PalindromesTest {
     true, false, true, false, true, true
   };
 
+  static final String[] denormalizeInputs = {
+      "radar",
+      "Radar",
+      "A man, a plan, a canal - Panama!",
+      "A man, a plan, a dam - Hoover!",
+      "aBbA"
+  };
+
+  static final boolean[] denormalizeExpecteds = {
+      true, true, true, false, true
+  };
+
   @Test
-  void computeRecursive() {
+  void testRecursive() {
     for (int i = 0; i < inputValues.length; i++) {
-      assertEquals(expectedValues[i], Palindromes.computeRecursive(inputValues[i]));
+      assertEquals(expectedValues[i], Palindromes.testRecursive(inputValues[i]));
     }
   }
 
   @Test
-  void computeIterative() {
+  void testDenormalized() {
+    for (int i = 0; i < denormalizeInputs.length; i++) {
+      assertEquals(denormalizeExpecteds[i], Palindromes.testDenormalized(denormalizeInputs[i]));
+    }
+  }
+
+  @Test
+  void testIterative() {
     for (int i = 0; i < inputValues.length; i++) {
-      assertEquals(expectedValues[i], Palindromes.computeIterative(inputValues[i]));
+      assertEquals(expectedValues[i], Palindromes.testIterative(inputValues[i]));
     }
   }
 
